d3948b530f697ff63df46ae182be5667edf4a97c
elilliu <elilliu@student.42.fr>
2024-07-10 20:56:35 +0200

    on a des threads c est super

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: elilliu <elilliu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 19:32:27 by elilliu           #+#    #+#             */
-/*   Updated: 2024/07/08 17:59:52 by elilliu          ###   ########.fr       */
+/*   Updated: 2024/07/10 20:46:07 by elilliu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,26 @@
 
 typedef struct s_philo
 {
-	int	num;
-	int	die;
-	int	eat;
-	int	sleep;
-	int	meals;
-	int	left_fork;
-	int	right_fork;
+	pthread_t	thread;
+	int			num;
+	int			die;
+	int			eat;
+	int			sleep;
+	int			meals;
+	int			left_fork;
+	int			right_fork;
 }		t_philo;
 
 typedef struct s_data
 {
-	int		nb_of_philo;
-	size_t	time_to_die;
-	size_t	time_to_eat;
-	size_t	time_to_sleep;
-	int		nb_of_meals;
+	int				nb_of_philo;
+	size_t			time_to_die;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	int				nb_of_meals;
+	int				current;
+	t_philo			*philo;
+	pthread_mutex_t	mutex;
 }	t_data;
 
 void	take_fork(int num);
@@ -44,5 +48,6 @@ void	eating(int num);
 void	sleeping(int num);
 void	thinking(int num);
 void	death(int num);
+long	ft_atoi(const char *nptr);
 
 #endif
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,26 +6,69 @@
 /*   By: elilliu <elilliu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 19:40:15 by elilliu           #+#    #+#             */
-/*   Updated: 2024/07/08 18:01:28 by elilliu          ###   ########.fr       */
+/*   Updated: 2024/07/10 20:54:38 by elilliu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
+void	*routine(void *structure)
+{
+	t_data	*data;
+	t_philo	philo;
+
+	data = (t_data *)structure;
+	philo = data->philo[data->current];
+	if (pthread_mutex_lock(&data->mutex))
+	{
+		printf("error unlocking mutex\n");
+		return (NULL);
+	}
+	printf("je suis le philosophe numero %d\n", philo.num);
+	pthread_mutex_unlock(&data->mutex);
+	return (NULL);
+}
+
 void	data_init(t_data *data, int ac, char **av)
 {
-	data->nb_of_philo = av[1];
-	data->time_to_die = (size_t)av[2];
-	data->time_to_eat = (size_t)av[3];
-	data->time_to_sleep = (size_t)av[4];
+	data->nb_of_philo = ft_atoi(av[1]);
+	data->time_to_die = (size_t)ft_atoi(av[2]);
+	data->time_to_eat = (size_t)ft_atoi(av[3]);
+	data->time_to_sleep = (size_t)ft_atoi(av[4]);
 	if (ac == 6)
-		data->nb_of_meals = av[5];
+		data->nb_of_meals = ft_atoi(av[5]);
+}
+
+void	init_philo(t_data *data, int i)
+{
+	data->philo[i].num = i + 1;
+	data->current = i;
+	pthread_create(&data->philo[i].thread, NULL, routine, (void *)data);
+	usleep(1000);
 }
 
 int	main(int ac, char **av)
 {
-	t_data			data;
-	pthread_mutex_t	mutex;
+	t_data		data;
+	int			i;
 
 	data_init(&data, ac, av);
+	pthread_mutex_init(&data.mutex, NULL);
+	i = 0;
+	data.philo = malloc(sizeof(t_philo) * data.nb_of_philo);
+	if (!data.philo)
+		return (1);
+	while (i < data.nb_of_philo)
+	{
+		init_philo(&data, i);
+		i++;
+	}
+	i = 0;
+	while (i < data.nb_of_philo)
+	{
+		pthread_join(data.philo[i].thread, NULL);
+		i++;
+	}
+	pthread_mutex_destroy(&data.mutex);
+	return (0);
 }
